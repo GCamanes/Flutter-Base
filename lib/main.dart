3c1b187f519b015dca18c57a84134d0ef306d733
gcamanes <guillaume.camanes@mobiapps.fr>
2022-01-10 09:48:49 +0100

    removed debug banner

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,8 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Flutter Base',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         // This is the theme of your application.
         //
