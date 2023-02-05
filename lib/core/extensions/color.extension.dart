8d2165c414e6451cd181829b356a160e31e1920d
gcamanes <guillaume.camanes.prestataire@wiztivi.com>
2023-02-05 20:39:42 +0100

    fixed dart analysis

--- lib/core/extensions/color.extension.dart
+++ lib/core/extensions/color.extension.dart
@@ -2,14 +2,14 @@ import 'package:flutter/material.dart';
 
 extension ColorExtension on Color {
   MaterialColor get material {
-    List strengths = <double>[.05];
-    Map<int, Color> swatch = {};
+    List<double> strengths = <double>[.05];
+    Map<int, Color> swatch = <int, Color>{};
     final int r = red, g = green, b = blue;
 
     for (int i = 1; i < 10; i++) {
       strengths.add(0.1 * i);
     }
-    for (var strength in strengths) {
+    for (double strength in strengths) {
       final double ds = 0.5 - strength;
       swatch[(strength * 1000).round()] = Color.fromRGBO(
         r + ((ds < 0 ? r : (255 - r)) * ds).round(),
@@ -20,7 +20,7 @@ extension ColorExtension on Color {
     }
     return MaterialColor(value, swatch);
   }
-  
+
   Color get emphasisHigh => withOpacity(0.87);
   Color get emphasisMedium => withOpacity(0.60);
   Color get emphasisDisabled => withOpacity(0.38);
